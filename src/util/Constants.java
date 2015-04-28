0be747c759b4aff8e10a22e495cfd208569bd653
Galen Scovell <galen.scovell@gmail.com>
2015-04-27 21:02:12 -0700

    Update documentation; begin LWJGL port

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -10,22 +10,22 @@ package util;
 public class Constants {
     private Constants() { }
 
-    // Width/height (in pixels) of application window
+    // Width/height of application window (in pixels) 
     public static final int WINDOW_X = 960;
     public static final int WINDOW_Y = 676;
 
-    // Width/height of world in pixels
+    // Width/height of world (in pixels)
     public static final int WORLD_WIDTH = 4800;
     public static final int WORLD_HEIGHT = 4800;
 
     // Tile square dimensions (in pixels)
     public static final int TILESIZE = 48;
 
-    // Tile columns/rows in world
+    // Columns/rows in world (in Tiles)
     public static final int TILE_COLUMNS = WORLD_WIDTH / TILESIZE;
     public static final int TILE_ROWS = WORLD_HEIGHT / TILESIZE;
 
-    // Height (in pixels) of HUD (HUD uses window width)
+    // Height of HUD (in pixels) 
     public static final int HUD_HEIGHT = TILESIZE * 2;
 
     // Renders per second = Framerate
